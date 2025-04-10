5744351c59dedd6aaa554501b1f450263781ba16
Eunji <ejyu710@gmail.com>
2025-04-10 10:47:27 +0100

    I modified common.css, index.html, game.html, index.js, game.js and added image.

--- javascript/game.js
+++ javascript/game.js
@@ -1,4 +1,57 @@
-function hello() 
-		{
-			alert("game page");
-		}
+// Create by Eunji
+
+$(document).ready(function () {
+
+  // 3 questions in the quiz
+  const quiz = [
+    { question: "Do you like going for walks outside?", yes: 2, no: 1 },
+    { question: "Do you prefer a quiet environment?", yes: 1, no: 2 },
+    { question: "Do you enjoy play time and physical activity?", yes: 2, no: 1 }
+  ];
+
+  let score = 0, current = 0;
+
+  // Show the current question or display the result if the quiz is finished
+  function showQuestion() {
+    if (current < quiz.length) {
+      $("#quizQ, #yesBtn, #noBtn").show();
+      $("#quizQ").text(quiz[current].question);
+      $("#restartBtn, #resultText").hide();
+    } 
+    else {
+      
+      let result = "";
+      if(score >= 5)
+        result = "Dog 🐶";
+      else
+        result = "Cat 🐱";
+
+      $("#resultText").text("Your perfect match is: " + result).show();
+      $("#quizQ, #yesBtn, #noBtn").hide();
+      $("#restartBtn").show();
+    }
+  }
+
+
+  // Handle score and move to next question on button click
+  $("#yesBtn").click(function(){
+    score += quiz[current].yes;
+    current++;
+    showQuestion();
+  });
+
+  $("#noBtn").click(function(){
+    score += quiz[current].no;
+    current++;
+    showQuestion();
+  });
+
+  $("#restartBtn").click(function () {
+    score = 0;
+    current = 0;
+    showQuestion();
+  });
+
+  // Call the function on initial page load
+  showQuestion();
+});
--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,13 @@
+// Create by Eunji
+
 $(document).ready(function () {
-      $('#testBtn').click(function () {
-        alert("Thanks for visiting My Pet Story! testing");
-      });
+
+    // Random animals
+    const pets = ["Cat🐈", "Dog🐶", "Rabbit🐰", "Parrot🦜"];
+
+    // Generate and display a random pet when the button is clicked
+    $("#randomBtn").click(function () {
+      const pick = pets[Math.floor(Math.random() * pets.length)];
+      $("#randomPet").text("Today's recommended pet: " + pick);
     });
+});
